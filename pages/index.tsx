a82f46ec733134daa06c527697c55c3af978b46d
fdsfdss <148657533+fdsfdss@users.noreply.github.com>
2023-11-10 22:40:07 +0000

    Update 3 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,22 +2,22 @@ import Head from "next/head";
 import Yolo from "../components/models/Yolo";
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import Image from 'next/image'; // Import the Image component from Next.js
+import icon from 'public/icon.png'; // Import your icon (adjust the path as needed)
 
 export default function Home() {
   return (
     <>
-      <main className="font-mono flex flex-col justify-center items-center  w-screen">
-        <h1 className="m-5 text-xl font-bold">Real-Time Object Detection</h1>
-        <Yolo />
-        <p className="m-5">
-          
-          <a
-            className="underline underline-offset-1 hover:translate-y-1"
-            href="https://masyen.com.tr/"
-          >
-            Masyen Muhendislik
+      <main className="font-mono flex flex-col justify-center items-center w-screen">
+        <div className="flex items-center justify-center">
+          {/* Wrap the Image component in an anchor tag */}
+          <a href="https://masyen.com.tr/" target="_blank" rel="noopener noreferrer">
+            <Image src={icon} alt="Masyen Logo" width={50} height={50} />
           </a>
-        </p>
+
+          <h1 className="m-5 text-xl font-bold">Masyen Object Detection</h1>
+        </div>
+        <Yolo />
       </main>
     </>
   );
